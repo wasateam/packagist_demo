6be4322a977edd33b081bbb16dc67021de8bb9c4
林典群 <davidturtle0313@gmail.com>
2020-06-06 22:58:40 +0800

    demo test;

---
+++ README.md
@@ -0,0 +1,2 @@
+just a demo
+so poor
---
+++ src/DemoClass.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Wasateam\Packagistdemo;
+
+use App\Http\Controllers\Controller;
+
+class DemoClass extends Controller
+{
+  public static function test()
+  {
+    error_log('test');
+  }
+}
